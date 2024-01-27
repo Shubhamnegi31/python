a3034b4e8df1990b8ee5e37b8556c6bf3ab3c45c
Shubhamnegi31 <157865450+Shubhamnegi31@users.noreply.github.com>
2024-01-27 09:39:33 +0530

    Add files via upload

---
+++ code.py1.py
@@ -0,0 +1,72 @@
+
+#a=float(input("enter first number"))
+#b=float(input("enter another number"))
+#c=a+b
+#d=a-b
+#e=a*b
+#f=a/b
+#g=a%b
+#h=a//b
+#i=a**b
+# print(f' The sum of number is {c} \n the diffrence of number is {d} \n the multiplication of number is {e} \n the divison of number is {f} \n the remainder of number is {g} \n the divison without decimals number is{ h} \n the power of number is {i}')
+#x=10
+#if not x:
+  #  print("ok")
+#if not (x%3==0 or x%5==0):
+ #   print("not ok")
+#else:
+ #   print("done")
+#def order(x):
+ #   print("method called for value",x)
+  #  return True if x>0 else False
+#a=order
+#b=order
+#c=order
+#if a(-1) or b(5) or (10):
+ #   print ("aleast one of the num is +ve")
+
+#temp=int(input("enter the temp"))
+#if temp>=30:
+ #   print("its too hot today")
+  #  print("take enough fluid")
+#if temp<=30:
+ #   print("its awesome")
+#if temp<=20:
+ #   print("its cool")
+#    print("enjoy the weather")
+#grade=input("Enter your grade \n")
+#if grade.upper()== 'O':
+ #   print ("you are eligible for   all specification")
+#if grade.upper()=='A':
+ #  print ("you are eligibe  for all  Specialisation  except AIML")
+#if grade.upper()=='B':
+ #  print ("you are eligible for all specialisation except AIML AND CLOUD COMPUTING"
+         # )
+#if grade.upper()=='C':
+ #  print ("you are eligible for all specialisation except AIMLCLOUD COMPUTING AND IBM")
+#else:
+ #  print("first focus on normal studies")
+
+#weight = int(input("enter the weight in kg"))
+#gram= weight*1000
+
+#print(f"the weight in grams is{gram}")
+num = int(input("We need you to guess a number: "))
+
+if num == 12:
+    print("Excellent, you guessed it right")
+else:
+    print("No, it's a wrong guess")
+    no = int(input("We need you to guess a number again: "))
+    
+    if no == 12:
+        print("Great, your second guess is right")
+    else:
+        print("No, it's a wrong guess")
+        nu = int(input("We need you to guess a number again: "))
+        
+        if nu == 12:
+            print("Bravo, your third guess is right")
+        else:
+            print("Oops, your guess chances are over. Better luck next time.")
+ 
